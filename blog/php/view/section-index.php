bef28102eb818b78c198cc988bfad6494b3e904c
webforce3 <lh@nomail.me>
2019-11-22 12:25:14 +0100

    Ajout flex et media queries

--- blog/php/view/section-index.php
+++ blog/php/view/section-index.php
@@ -1,6 +1,9 @@
-
 <section>
     <h2>Accueil</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusantium, suscipit et maxime libero, veritatis obcaecati sapiente voluptatum sequi facilis odio dolores distinctio debitis. Quis quia veritatis nisi quod vel?</p>
-    <img src="assets/img/photo1.jpg" alt="cuisine">
+    <div class="ligne x3col">
+        <img src="assets/img/photo1.jpg" alt="cuisine">
+        <img src="assets/img/photo2.jpg" alt="cuisine">
+        <img src="assets/img/photo3.jpg" alt="cuisine">
+    </div>
 </section>
