45c565e962a4ae7d0b609ed292a9340177341899
webforce3 <lh@nomail.me>
2019-11-22 14:38:25 +0100

    ajout html et css

--- blog/php/view/section-recettes.php
+++ blog/php/view/section-recettes.php
@@ -1,3 +1,38 @@
+<section>
+    <h2>mes dernières recettes</h2>
+    <div class="ligne x3col listeRecette">
+        <article>
+            <img src="assets/img/photo1.jpg" alt="photo">
+            <h3>recette1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt at molestiae quisquam minima asperiores vitae. Possimus accusantium natus et aliquid amet aliquam harum. Quasi at aspernatur ab impedit cumque minus.</p>
+        </article>
+        <article>
+            <img src="assets/img/photo2.jpg" alt="photo">
+            <h3>recette2</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt at molestiae quisquam minima asperiores vitae. Possimus accusantium natus et aliquid amet aliquam harum. Quasi at aspernatur ab impedit cumque minus.</p>
+        </article>
+        <article>
+            <img src="assets/img/photo3.jpg" alt="photo">
+            <h3>recette3</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt at molestiae quisquam minima asperiores vitae. Possimus accusantium natus et aliquid amet aliquam harum. Quasi at aspernatur ab impedit cumque minus.</p>
+        </article>
+        <article>
+            <img src="assets/img/photo4.jpg" alt="photo">
+            <h3>recette4</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt at molestiae quisquam minima asperiores vitae. Possimus accusantium natus et aliquid amet aliquam harum. Quasi at aspernatur ab impedit cumque minus.</p>
+        </article>
+        <article>
+            <img src="assets/img/photo5.jpg" alt="photo">
+            <h3>recette5</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt at molestiae quisquam minima asperiores vitae. Possimus accusantium natus et aliquid amet aliquam harum. Quasi at aspernatur ab impedit cumque minus.</p>
+        </article>
+        <article>
+            <img src="assets/img/photo6.jpg" alt="photo">
+            <h3>recette6</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt at molestiae quisquam minima asperiores vitae. Possimus accusantium natus et aliquid amet aliquam harum. Quasi at aspernatur ab impedit cumque minus.</p>
+        </article>
+    </div>
+</section>
 
 <section>
     <h2>Recettes</h2>
