e8f8ce0efe83a10738aa45cf5df3bd92001a414f
webforce3 <lh@nomail.me>
2019-12-04 17:20:38 +0100

    ajout doc

--- cms/php/controller/traitement-contenu.php
+++ cms/php/controller/traitement-contenu.php
@@ -1,9 +1,9 @@
 <?php
 
-$titre          = filtrerTexte("titre");
-$photo          = filtrerUpload("photo");
-$description    = filtrerTexte("description", 1, 10000);
-$categorie      = filtrerTexte("categorie");
+$titre          = filtrerTexte("titre");                    // <input type="text" name="titre">
+$photo          = filtrerUpload("photo");                   // <input type="file" name="photo">
+$description    = filtrerTexte("description", 1, 10000);    // <textarea name="description"></textarea>
+$categorie      = filtrerTexte("categorie");                // <input type="text" name="categorie">
 
 if (count($tabErreur) == 0) {
     $datePublication = date("Y-m-d H:i:s"); // FORMAT DATETIME SQL
