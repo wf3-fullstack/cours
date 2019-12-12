c5cd5c0cca8cf18b3c4e4cf8edbdccb9306c1ae3
webforce3 <lh@nomail.me>
2019-12-12 16:57:32 +0100

    ajout correction exo3.php et exo4.php

--- poo01/exo3.php
+++ poo01/exo3.php
@@ -1,15 +1,56 @@
 <?php
 
+
+// SI BESOIN
+// CONFIGURER L'AFFICHAGE DES ERREURS
+// AFFICHER LES ERREURS PHP
+// https://www.php.net/manual/fr/function.error-reporting.php
+// https://www.php.net/manual/fr/function.ini-set.php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 // AJOUTER LE CODE MANQUANT EN POO
 // POUR AFFICHER
 // (header)
 // (LE CONTENU DE MA PAGE)
 // (footer)
 
+// Fatal error: Uncaught Error: Class 'Page' not found
+class Page 
+{
+    // JE CREE UNE PROPRIETE D'OBJET
+    // ELLE EXISTE TANT QUE L'OBJET EXISTE
+    // => PRATIQUE CA PERMET DE PARTAGER UNE INFORMATIONS ENTRE PLUSIEURS APPELS DE METHODE
+    public $content = "";
 
+    // Fatal error: Uncaught Error: Call to undefined method Page::setContenu()
+    // => METHODE SETTER => ECRITURE
+    function setContenu ($content)
+    {
+        // JE DOIS MEMORISER LA VALEUR DE $content
+        // POUR POUVOIR L'UTILISER PLUS TARD DANS UNE AUTRE METHODE
+        // ON VA UTILISER UNE VARIABLE
+        // => EN FONCTIONNEL
+        // locale => PAS BON DETRUIT A LA FIN DE LA FONCTION
+        // static => PAS BON GARDE A L'INTERIEUR DE LA FONCTION setContenu 
+        // globale => PAS BON PARCE QU'ON VEUT FAIRE DE LA POO
+        // => EN POO
+        // propriété d'objet à la classe
+        // NOTE: $this DONNE L'OBJET QUI A SERVI A APPELER LA METHODE
+        $this->content = $content;
+    }    
+
+    // Fatal error: Uncaught Error: Call to undefined method Page::afficher()
+    function afficher ()
+    {
+        echo "<h1>(header)</h1>";
+        echo "<h1>{$this->content}</h1>";
+        echo "<h1>(footer)</h1>";
+    }
+}
 
 
 // CODE NON MODIFIABLE
-$objetPage = new Page;
-$objetPage->setContenu("LE CONTENU DE MA PAGE");
-$objetPage->afficher();
+$objetPage = new Page;                              // ON PEUT JOUER AVEC __construct
+$objetPage->setContenu("LE CONTENU DE MA PAGE");    // $this = $obejtPage
+$objetPage->afficher();                             // $this = $objetPage
--- poo01/exo4.php
+++ poo01/exo4.php
@@ -1,5 +1,13 @@
 <?php
 
+// SI BESOIN
+// CONFIGURER L'AFFICHAGE DES ERREURS
+// AFFICHER LES ERREURS PHP
+// https://www.php.net/manual/fr/function.error-reporting.php
+// https://www.php.net/manual/fr/function.ini-set.php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 // AJOUTER LE CODE MANQUANT EN POO
 // POUR AFFICHER
 /*
@@ -11,13 +19,53 @@
 (footer)
 
 */
+// Fatal error: Uncaught Error: Class 'Page' not found
+class Page 
+{
+    // PROPRIETE D'OBJET
+    //public $content = "";
+    public $tabContent = [];
+
+    function __construct ()
+    {
+        //echo "(header)";
+    }
+
+    function __destruct()
+    {
+    }
+
+    // Fatal error: Uncaught Error: Call to undefined method Page::ajouterContenu()
+    function ajouterContenu ($content)
+    {
+        // echo "($content)";
+        //$this->content .= "($content)";
+        $this->tabContent[] = $content;
+    }
+
+    // Fatal error: Uncaught Error: Call to undefined method Page::afficherListe()
+    function afficherListe ()
+    {
+        echo "(header)";
+        // JE DOIS AFFICHER LES 3 SECTIONS
+        // echo "{$this->content}";
+        foreach($this->tabContent as $content)
+        {
+            echo "($content)";
+        }
+        echo "(footer)";
+    }
+}
 
 
 
 // CODE NON MODIFIABLE
-$objetPage = new Page;
+$objetPage = new Page;                      // __construct
 $objetPage->ajouterContenu("SECTION1");
 $objetPage->ajouterContenu("SECTION2");
 $objetPage->ajouterContenu("SECTION3");
 $objetPage->afficherListe();
+
+// DESTRUCTION                              // _destruct
+
 
